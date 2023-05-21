5b479658eed14cc016f06461640df190f6fa873a
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-05-21 18:30:20 +0900

    Session!

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,9 +9,13 @@ class SessionsController < ApplicationController
     end
 
     user = AuthenticationProviderGithub.find_or_create_user_from_auth_hash(request.env["omniauth.auth"])
+    session[:user_id] = user.id
+
     redirect_to root_path
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to login_path
   end
 end
