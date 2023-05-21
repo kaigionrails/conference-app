5cd1975a7418ef1e929e1695c4d90f4a1e2ab579
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-05-22 02:42:58 +0900

    Fetch profile image from github

--- Gemfile
+++ Gemfile
@@ -49,6 +49,8 @@ gem "omniauth"
 gem "omniauth-rails_csrf_protection"
 gem "omniauth-github"
 
+gem "octokit"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,17 @@
 class Profile < ApplicationRecord
   belongs_to :user
   has_many_attached :images
+
+  def ensure_image_from_github
+    profile_image = fetch_profile_image_from_github
+    m = Marcel::Magic.by_magic profile_image
+    images.attach(io: profile_image, filename: "github.#{m.subtype}")
+  end
+
+  private def fetch_profile_image_from_github
+    client = Octokit::Client.new
+    url = client.user(user.authentication_provider_github.uid.to_i).avatar_url
+    require "open-uri"
+    URI.open(url)
+  end
 end
