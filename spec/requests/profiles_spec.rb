af10a5c996a596f3852cbc2c436ec18612a61846
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-05-22 02:07:10 +0900

    Basic request spec

--- spec/requests/profiles_spec.rb
+++ spec/requests/profiles_spec.rb
@@ -1,25 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe "Profiles", type: :request do
+  let(:user) { FactoryBot.create(:user) }
+
   describe "GET /index" do
+    before do
+      sign_in(user)
+    end
+
     it "returns http success" do
-      get "/profiles/index"
+      get "/profiles"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /new" do
+    before do
+      sign_in(user)
+    end
+
     it "returns http success" do
       get "/profiles/new"
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/profiles/create"
-      expect(response).to have_http_status(:success)
+  describe "POST /profiles" do
+    before do
+      sign_in(user)
+    end
+
+    context "valid profile" do
+      it "should create profile" do
+        post "/profiles", params: { profile: { name: "foobar", description: "I'm foobar!" } }
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(profiles_path)
+        expect(Profile.where(user: user).count).to eq 1
+      end
     end
   end
-
 end
