564f36403b3559e660b383de16cda3b7db354c3f
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-05-21 18:33:49 +0900

    Create home controller and set it to root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "home#index"
+
   get "/login", to: "sessions#new"
   get "/auth/:provider/callback", to: "sessions#create"
   get "/logout", to: "sessions#destroy"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
