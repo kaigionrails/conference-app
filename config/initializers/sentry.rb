a5c6cd24f8f395a09849c06161288d6de0f2e010
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-10-11 01:44:49 +0900

    Add sentry document url

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,3 +1,4 @@
+# https://docs.sentry.io/platforms/ruby/guides/rails/
 Sentry.init do |config|
   config.dsn = ENV["SENTRY_DSN"]
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
