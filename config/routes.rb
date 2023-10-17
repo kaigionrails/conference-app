daa7503dbb2dcc535a80ec3179eee81ef3901b96
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-10-18 01:17:03 +0900

    Tell pwa-ready to web browser

--- config/routes.rb
+++ config/routes.rb
@@ -18,8 +18,8 @@ Rails.application.routes.draw do
   get "/logout", to: "sessions#destroy"
 
   # For PWA
-  get "/sw.js", to: "pwa#service_worker"
-  get "/manifest.json", to: "pwa#manifest"
+  get "/sw.js", to: "pwa#service_worker", as: "service_worker"
+  get "/manifest.json", to: "pwa#manifest", as: "pwa_manifest"
 
   resources :profiles, only: [:index, :new, :create, :edit, :update] do
     resources :profile_images, only: [:destroy]
