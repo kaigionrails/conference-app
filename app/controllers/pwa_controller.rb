43f85c8ec4392dd7f59ad3d5e8b824bab989f0e5
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-10-17 11:33:11 +0900

    Return service worker from rails
    To modify `Cache-Control` header

---
+++ app/controllers/pwa_controller.rb
@@ -0,0 +1,8 @@
+class PwaController < ApplicationController
+  protect_from_forgery except: :service_worker
+  layout false
+
+  def service_worker
+    response.headers['Cache-Control'] = "no-cache"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,9 @@ Rails.application.routes.draw do
   get "/auth/:provider/callback", to: "sessions#create"
   get "/logout", to: "sessions#destroy"
 
+  # For PWA
+  get "/sw.js", to: "pwa#service_worker"
+
   resources :profiles, only: [:index, :new, :create, :edit, :update] do
     resources :profile_images, only: [:destroy]
   end
