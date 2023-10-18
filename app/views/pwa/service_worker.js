f1d6c178e3dd3d1a44dd6d73bee0298795ef5fb0
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-10-18 17:27:12 +0900

    Add push event handler to service worker
    show webpush notification

--- app/views/pwa/service_worker.js
+++ app/views/pwa/service_worker.js
@@ -2,4 +2,17 @@ const installHandler = async (event) => {
   console.info(event);
 };
 
+const pushHandler = async (event) => {
+  const notificationData = event.data.json();
+  const notificationPromise = self.registration.showNotification(
+    notificationData.title,
+    {
+      body: notificationData.body,
+      icon: notificationData.icon,
+    }
+  );
+  event.waitUntil(notificationPromise);
+};
+
 self.addEventListener("install", installHandler);
+self.addEventListener("push", pushHandler);
