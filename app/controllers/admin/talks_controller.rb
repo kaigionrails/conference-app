ee130ada22ed4e6e90825d2f2838f95f9c028d05
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-10-20 03:02:34 +0900

    Create admin/talks#show view

--- app/controllers/admin/talks_controller.rb
+++ app/controllers/admin/talks_controller.rb
@@ -2,4 +2,8 @@ class Admin::TalksController < AdminController
   def index
     @talks = Talk.eager_load(:speakers).order(:start_at).page(params[:page]).per(50)
   end
+
+  def show
+    @talk = Talk.eager_load(:speakers).find(params[:id])
+  end
 end
