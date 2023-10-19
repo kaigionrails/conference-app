53f59d46cb3709bdadb4695734eff4cac3a7d543
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-10-20 02:19:00 +0900

    Create admin/users#show view

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,8 @@ class Admin::UsersController < AdminController
   def index
     @users = User.eager_load(:profile).order(:id).page(params[:page])
   end
+
+  def show
+    @user = User.eager_load(:profile).find(params[:id])
+  end
 end
