7a0868b1ddab6ed41efacc47921190ea16039e63
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-10-20 00:53:08 +0900

    Create AdminController and restrict access

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,15 @@
+class AdminController < ApplicationController
+  before_action :require_organizer
+
+  def index
+  end
+
+  private def require_organizer
+    if logged_in? && current_user.organizer?
+      # pass
+    else
+      flash.now[:alert] = "Require organizer role"
+      redirect_to root_path
+    end
+  end
+end
