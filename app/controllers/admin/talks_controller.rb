427e2d6424b40a5a4e0dca56e0ccf170cbab63b6
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-10-20 17:09:49 +0900

    Editable talk on admin/talks

--- app/controllers/admin/talks_controller.rb
+++ app/controllers/admin/talks_controller.rb
@@ -6,4 +6,27 @@ class Admin::TalksController < AdminController
   def show
     @talk = Talk.eager_load(:speakers).find(params[:id])
   end
+
+  def edit
+    @talk = Talk.find(params[:id])
+  end
+
+  def update
+    talk = Talk.find(params[:id])
+    update_params = talk_params.dup
+    start_at_date = update_params.delete(:start_at_date)
+    start_at_time = update_params.delete(:start_at_time)
+    start_at_with_zone = Time.zone.parse("#{start_at_date} #{start_at_time} +0900")
+    if talk.update(**update_params, start_at: start_at_with_zone)
+      flash.now[:success] = "Update succeeded"
+      redirect_to admin_talk_path(talk)
+    else
+      flash.now[:alert] = "Update failed"
+      redirect_to edit_admin_talk_path(talk)
+    end
+  end
+
+  private def talk_params
+    params.require(:talk).permit(:title, :abstract, :start_at_date, :start_at_time, :track, :dutation_minutes)
+  end
 end
