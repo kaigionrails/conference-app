585a123e98901623de8a3ed0c1e3a3b8bd21cef4
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-10-21 16:46:34 +0900

    Create talks#show view

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -1,6 +1,11 @@
 class TalksController < ApplicationController
   def index
+    @event = Event.find_by!(slug: params[:event_slug])
+    @talks = Talk.eager_load(:speakers).where(event: @event).order(:start_at)
+  end
+
+  def show
     event = Event.find_by!(slug: params[:event_slug])
-    @talks = Talk.eager_load(:speakers).where(event: event).order(:start_at)
+    @talk = Talk.eager_load(:speakers).find_by!(id: params[:id])
   end
 end
