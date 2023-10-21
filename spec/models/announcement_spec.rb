0237e8841a28551468d1dc55001acda7bbb29f30
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-10-22 02:09:21 +0900

    Add validation and callback to announcement model

--- spec/models/announcement_spec.rb
+++ spec/models/announcement_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Announcement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "status" do
+    context "already published" do
+      let(:announcement) { FactoryBot.create(:announcement, status: "published", published_at: Time.current) }
+      it "shouldn't change status to draft" do
+        announcement.status = "draft"
+        expect(announcement).to be_invalid
+     end
+    end
+
+    context "when publish" do
+      let(:announcement) { FactoryBot.create(:announcement, status: "draft") }
+      it "should fill published_at timestamp" do
+        announcement.published!
+        expect(announcement.published_at).to be_a Time
+        expect(announcement.published_at).to be <= Time.current
+      end
+    end
+  end
 end
