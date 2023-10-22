0017905bdd27478919c95fde391768966aff0ef7
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-10-23 02:07:08 +0900

    Show/edit/update announcement on admin console

--- app/controllers/admin/announcements_controller.rb
+++ app/controllers/admin/announcements_controller.rb
@@ -17,6 +17,21 @@ class Admin::AnnouncementsController < AdminController
     redirect_to admin_announcements_path
   end
 
+  def edit
+    @announcement = Announcement.find(params[:id])
+  end
+
+  def update
+    announcement = Announcement.find(params[:id])
+    if announcement.update(**announcement_params)
+      flash.now[:success] = "Update succeeded"
+      redirect_to admin_announcement_path(announcement)
+    else
+      flash.now[:alert] = "Update failed"
+      redirect_to edit_admin_announcement_path(announcement)
+    end
+  end
+
   private def announcement_params
     params.require(:announcement).permit(:event_id, :title, :content, :status)
   end
