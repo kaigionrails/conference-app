1893c1dc96409f688cbaead9ee614f41fb6fa44c
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-10-23 03:07:22 +0900

    Create model UnreadAnnouncement

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_22_034141) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_22_175913) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -120,6 +120,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_22_034141) do
     t.index ["event_id"], name: "index_talks_on_event_id"
   end
 
+  create_table "unread_announcements", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "announcement_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["announcement_id"], name: "index_unread_announcements_on_announcement_id"
+    t.index ["user_id", "announcement_id"], name: "index_unread_announcements_on_user_id_and_announcement_id", unique: true
+    t.index ["user_id"], name: "index_unread_announcements_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "role", default: "participant", null: false
@@ -141,5 +151,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_22_034141) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "authentication_provider_githubs", "users"
   add_foreign_key "profiles", "users"
+  add_foreign_key "unread_announcements", "announcements"
+  add_foreign_key "unread_announcements", "users"
   add_foreign_key "webpush_subscriptions", "users"
 end
