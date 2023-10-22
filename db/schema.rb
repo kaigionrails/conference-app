b06f50d9c349005d1da526afedbbd2e06d53ada4
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-10-22 12:45:33 +0900

    Add title column to announcement

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_21_172536) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_22_034141) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,6 +58,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_21_172536) do
     t.datetime "published_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title", null: false
     t.index ["event_id"], name: "index_announcements_on_event_id"
   end
 
