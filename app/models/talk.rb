ab7317e3a39474d450f59aae150d9a3630de5fe5
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-10-23 11:45:09 +0900

    Create `bookmarked_by?` and `bookmark_by` to talk model

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -2,4 +2,12 @@ class Talk < ApplicationRecord
   belongs_to :event
   has_and_belongs_to_many :speakers
   has_many :talk_bookmarks, dependent: :destroy
+
+  def bookmarked_by?(user)
+    talk_bookmarks.where(user: user).exists?
+  end
+
+  def bookmark_by(user)
+    talk_bookmarks.find_by(user: user)
+  end
 end
--- spec/models/talk_spec.rb
+++ spec/models/talk_spec.rb
@@ -1,5 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe Talk, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:talk) { FactoryBot.create(:talk) }
+
+  describe "#bookmarked_by?" do
+    context "user is nil" do
+      it "should return false" do
+        expect(talk.bookmarked_by?(nil)).to eq false
+      end
+    end
+
+    context "user is not bookmarked the talk" do
+      let(:user) { FactoryBot.create(:user) }
+      it "should return false" do
+        expect(talk.bookmarked_by?(user)).to eq false
+      end
+    end
+
+    context "user bookmarked the talk" do
+      let(:user) { FactoryBot.create(:user) }
+      let!(:bookmark) { FactoryBot.create(:talk_bookmark, user: user, talk: talk) }
+      it "should return false" do
+        expect(talk.bookmarked_by?(user)).to eq true
+      end
+    end
+  end
+
+  describe "#bookmark_by" do
+    context "user is nil" do
+      it "should return nil" do
+        expect(talk.bookmark_by(nil)).to eq nil
+      end
+    end
+
+    context "user is not bookmarked the talk" do
+      let(:user) { FactoryBot.create(:user) }
+      it "should return nil" do
+        expect(talk.bookmark_by(nil)).to eq nil
+      end
+    end
+
+    context "user bookmarked the talk" do
+      let(:user) { FactoryBot.create(:user) }
+      let!(:bookmark) { FactoryBot.create(:talk_bookmark, user: user, talk: talk) }
+      it "should return false" do
+        expect(talk.bookmark_by(user)).to eq bookmark
+      end
+    end
+  end
 end
