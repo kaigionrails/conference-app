519dcd5fc322459538d8d3f4db04195c9cae617d
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-10-24 03:00:21 +0900

    Update factories user and profile
    with profile image

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,11 @@ FactoryBot.define do
   factory :user do
     name { "foo" }
     role { "participant" }
+
+    trait :with_profile_image do
+      after(:create) do |user|
+        create(:profile, :with_image, user: user)
+      end
+    end
   end
 end
