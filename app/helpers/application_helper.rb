dde66ee3169e90373ba325dd8a42a15ef72ef84f
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-10-23 16:05:55 +0900

    Add helper `current_event`

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def vapid_public_key_bytes
     @vapid_public_key_bytes ||= Base64.urlsafe_decode64(Rails.configuration.x.webpush.vapid_public_key).bytes
   end
+
+  def current_event
+    @current_event ||= Event.find_by(slug: Event::ONGOING_EVENT_SLUG)
+  end
 end
