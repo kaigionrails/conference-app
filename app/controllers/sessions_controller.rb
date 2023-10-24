7f4f286c5c434cad4930ac3d2b00780ac68b6078
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-10-24 23:47:02 +0900

    Redirect to return_to when callback from auth provider

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,12 @@ class SessionsController < ApplicationController
     user = AuthenticationProviderGithub.find_or_create_user_from_auth_hash(request.env["omniauth.auth"])
     session[:user_id] = user.id
 
-    redirect_to root_path
+    if request.env["omniauth.params"].key?("return_to")
+      # Prevent open redirect
+      redirect_to  URI.parse(request.env["omniauth.params"]["return_to"]).path
+    else
+      redirect_to root_path
+    end
   end
 
   def destroy
