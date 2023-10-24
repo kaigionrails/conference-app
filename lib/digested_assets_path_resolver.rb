a36d97801b5dcfed60bc7a2ba1713c05d31f2e22
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-10-24 16:59:51 +0900

    Make manifest_path to local var from instance var

--- lib/digested_assets_path_resolver.rb
+++ lib/digested_assets_path_resolver.rb
@@ -2,9 +2,9 @@ require "json"
 
 class DigestedAssetsPathResolver
   def initialize
-      @manifest_path = File.join("public", "assets", ".manifest.json")
-      @manifest = if File.exist?(@manifest_path)
-        JSON.parse(File.read(@manifest_path))
+      manifest_path = File.join("public", "assets", ".manifest.json")
+      @manifest = if File.exist?(manifest_path)
+        JSON.parse(File.read(manifest_path))
       else
         {}
       end
