326cdcf219d3269fea95d72458ba5db92d09c838
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-10-25 10:59:52 +0900

    Validation to color code in profile_badge

--- app/models/profile_badge.rb
+++ app/models/profile_badge.rb
@@ -1,3 +1,7 @@
 class ProfileBadge < ApplicationRecord
+  COLOR_CODE_REGEXP = %r[\A#\h{6}\z] # Color code e.g. #00FF22, #1234ab
   has_and_belongs_to_many :profiles
+
+  validates :background_color_code, format: { with: COLOR_CODE_REGEXP }
+  validates :border_color_code, format: { with: COLOR_CODE_REGEXP }
 end
