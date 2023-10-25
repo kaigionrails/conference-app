b62ca6cfb18ba4813412631ff57965b65383d1a9
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-10-26 01:26:35 +0900

    Preload attached blog in talks controller

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -1,11 +1,11 @@
 class TalksController < ApplicationController
   def index
     @event = Event.find_by!(slug: params[:event_slug])
-    @talks = Talk.eager_load(:speakers).where(event: @event).order(:start_at)
+    @talks = Talk.eager_load(speakers: { avatar_attachment: :blob }).where(event: @event).order(:start_at)
   end
 
   def show
     @event = Event.find_by!(slug: params[:event_slug])
-    @talk = Talk.eager_load(:speakers).find_by!(id: params[:id])
+    @talk = Talk.eager_load(speakers: { avatar_attachment: :blob }).find_by!(id: params[:id])
   end
 end
