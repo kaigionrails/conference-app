5806f4c27cff590726fd1a64ebdf6ca40d0b7734
megane42 <h.kzm.gm@gmail.com>
2023-10-29 16:11:39 +0900

    Remove verbosity about date

--- app/helpers/talks_helper.rb
+++ app/helpers/talks_helper.rb
@@ -2,8 +2,4 @@ module TalksHelper
   def time_with_zone_to_anchor(time_with_zone)
     time_with_zone.in_time_zone("Tokyo").strftime("%m%d-%H%M")
   end
-
-  def time_with_zone_to_header(time_with_zone)
-    time_with_zone.in_time_zone("Tokyo").strftime("%d日 %H:%M ～")
-  end
 end
