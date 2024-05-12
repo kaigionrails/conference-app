d36833885cfb6bb3cb5ae9b9c9d80c51d925df76
Yusuke Nakamura <yusuke1994525@gmail.com>
2024-05-12 15:19:51 +0900

    Update stimulus-use to v0.52.2
    stimulus-use from v0.52.0 dropped hotkeys-js dependency
    https://github.com/stimulus-use/stimulus-use/releases/tag/v0.52.0

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,8 +7,7 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "service_worker_installer", preload: true
 pin "stimulus-dropdown", to: "https://cdn.jsdelivr.net/npm/stimulus-dropdown@2.1.0/dist/stimulus-dropdown.mjs"
-pin "hotkeys-js", to: "https://cdn.jsdelivr.net/npm/hotkeys-js@3.12.0/dist/hotkeys.esm.js"
-pin "stimulus-use", to: "https://cdn.jsdelivr.net/npm/stimulus-use@0.51.3/dist/index.js"
+pin "stimulus-use", to: "https://cdn.jsdelivr.net/npm/stimulus-use@0.52.2/dist/index.js"
 pin "@rails/request.js", to: "https://cdn.jsdelivr.net/npm/@rails/request.js@0.0.9/dist/requestjs.min.js"
 pin "trix", to: "https://cdn.jsdelivr.net/npm/trix@2.0.7/dist/trix.esm.min.js"
 pin "@rails/actiontext", to: "actiontext.js"
