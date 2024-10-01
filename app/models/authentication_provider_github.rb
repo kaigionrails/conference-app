7c83208f1144ccca01011c417aed760b062920a0
Yusuke Nakamura <yusuke1994525@gmail.com>
2024-10-02 02:39:10 +0900

    :lipstick:

--- app/models/authentication_provider_github.rb
+++ app/models/authentication_provider_github.rb
@@ -1,18 +1,14 @@
 class AuthenticationProviderGithub < ApplicationRecord
   belongs_to :user
 
-  def self.find_or_create_user_from_auth_hash(auth, &block)
+  def self.find_or_create_user_from_auth_hash(auth, &)
     authentication_provider_github = AuthenticationProviderGithub.eager_load(:user).find_by(uid: auth.uid)
     return authentication_provider_github.user if authentication_provider_github
 
-    if block_given?
-      create_user_from_auth_hash(auth, &block)
-    else
-      create_user_from_auth_hash(auth)
-    end
+    create_user_from_auth_hash(auth, &)
   end
 
-  def self.create_user_from_auth_hash(auth)
+  def self.create_user_from_auth_hash(auth, &block)
     user = User.new(name: auth["info"]["nickname"], role: "participant")
     profile = Profile.new(user: user, name: user.name)
     auth_provider = AuthenticationProviderGithub.new(uid: auth["uid"], user: user)
@@ -21,7 +17,7 @@ class AuthenticationProviderGithub < ApplicationRecord
       auth_provider.save!
       profile.save!
     end
-    yield(user) if block_given?
+    yield(user) if block
     user
   end
 end
