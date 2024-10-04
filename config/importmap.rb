ba33e4d81f15ab7f560fc839dda15968a2500fda
Yusuke Nakamura <yusuke1994525@gmail.com>
2024-10-05 01:24:56 +0900

    Update trix to v2.1.6

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,7 +9,7 @@ pin "service_worker_installer", preload: true
 pin "stimulus-dropdown", to: "https://cdn.jsdelivr.net/npm/stimulus-dropdown@2.1.0/dist/stimulus-dropdown.mjs"
 pin "stimulus-use", to: "https://cdn.jsdelivr.net/npm/stimulus-use@0.52.2/dist/index.js"
 pin "@rails/request.js", to: "https://cdn.jsdelivr.net/npm/@rails/request.js@0.0.11/dist/requestjs.min.js"
-pin "trix", to: "https://cdn.jsdelivr.net/npm/trix@2.0.7/dist/trix.esm.min.js"
+pin "trix", to: "https://cdn.jsdelivr.net/npm/trix@2.1.6/dist/trix.esm.min.js"
 pin "@rails/actiontext", to: "actiontext.js"
 pin "qrcode", to: "https://ga.jspm.io/npm:qrcode@1.5.4/lib/browser.js"
 pin "dijkstrajs", to: "https://ga.jspm.io/npm:dijkstrajs@1.0.3/dijkstra.js"
