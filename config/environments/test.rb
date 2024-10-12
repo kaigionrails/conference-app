5fe4cc8b4294b1c0517875f090630fd5e6d9d60c
Yusuke Nakamura <yusuke1994525@gmail.com>
2024-10-13 05:09:55 +0900

    bundle exec standardrb --fix

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
 
   # Unlike controllers, the mailer instance doesn't have any context about the
   # incoming request so you'll need to provide the :host parameter yourself.
-  config.action_mailer.default_url_options = { host: "www.example.com" }
+  config.action_mailer.default_url_options = {host: "www.example.com"}
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
