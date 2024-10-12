cb2459b0919991641a26b64bde5cf2d61ed7ef08
Yusuke Nakamura <yusuke1994525@gmail.com>
2024-10-12 17:13:27 +0900

    Make idempotent seed script
    db:seed multiple time!

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,10 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-event_2023 = Event.find_or_create_by!(
-  name: "Kaigi on Rails 2023",
-  slug: "2023",
-  start_date: Time.zone.parse("2023-10-27 00:00:00 +0900"),
-  end_date: Time.zone.parse("2023-10-28 23:59:59 +0900")
-)
+event_2023 = Event.find_or_create_by!(name: "Kaigi on Rails 2023", slug: "2023") do |event|
+  event.start_date = Time.zone.parse("2023-10-27 00:00:00 +0900")
+  event.end_date = Time.zone.parse("2023-10-28 23:59:59 +0900")
+end
 
 if event_2023.talks.empty?
   talks_2023 = YAML.unsafe_load_file(Rails.root.join("db/seeds/2023.yaml"), symbolize_names: true)
