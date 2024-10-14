f5718027f6dd9a202ecf14307e788c6fd803e8ec
Yusuke Nakamura <yusuke1994525@gmail.com>
2024-10-14 15:32:35 +0900

    Def missing sigs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -24,7 +24,7 @@ class SessionsController < ApplicationController
       return
     end
 
-    session[:user_id] = user.id
+    session[:user_id] = user.id if user
 
     if params.key?("return_to")
       # Prevent open redirect
