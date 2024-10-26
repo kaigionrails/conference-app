f083020cb0131a35e51300b3a2504d7ee482a438
Takeshi KOMIYA <i.tkomiya@gmail.com>
2024-10-27 01:42:40 +0900

    sig: Migrate handwritten RBS files to RBS::Inline (others)

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,13 +1,18 @@
+# @rbs module-self ActionController::Base
 module SessionsHelper
   class UnauthorizedError < StandardError
   end
 
+  # @rbs @current_user: User
+
+  # @rbs return: User
   def current_user!
     raise UnauthorizedError unless session[:user_id]
 
     @current_user ||= User.find(session[:user_id])
   end
 
+  # @rbs return: bool
   def logged_in?
     current_user!.present?
   rescue UnauthorizedError
