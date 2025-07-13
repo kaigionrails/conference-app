0cb6d943041bf8b54981c72e47f1796b91903841
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-07-13 16:17:26 +0900

    [TMP]Change dir

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -2,10 +2,10 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
-    "./public/*.html",
-    "./app/helpers/**/*.rb",
-    "./app/javascript/**/*.js",
-    "./app/views/**/*.{erb,haml,html,slim}",
+    "../public/*.html",
+    "../app/helpers/**/*.rb",
+    "../app/javascript/**/*.js",
+    "../app/views/**/*.{erb,haml,html,slim}",
   ],
   theme: {
     extend: {
