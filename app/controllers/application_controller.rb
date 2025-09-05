7362e2d54a7181a764295042b6e57b3074d5a6ce
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-09-05 23:50:23 +0900

    Locale switcher

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,15 @@ class ApplicationController < ActionController::Base
 
   before_action :set_variant!
 
+  around_action :switch_locale
+
   def require_logged_in
     redirect_to login_path unless logged_in?
+  end
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
   end
 
   private def set_variant!
