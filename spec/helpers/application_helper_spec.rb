fe98a5990253bb47769bb3a9a8cb6b35deb555f0
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-09-18 11:30:03 +0900

    Fix css class name in spec

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe ApplicationHelper, type: :helper do
   describe "#message_type_style" do
     context "when message type is success" do
       it "returns success message style" do
-        expect(helper.message_type_style("success")).to eq("bg-[#d4edda]")
+        expect(helper.message_type_style("success")).to eq("bg-theme-success")
       end
     end
 
     context "when message type is alert" do
       it "returns alert message style" do
-        expect(helper.message_type_style("alert")).to eq("bg-[#f8d7da]")
+        expect(helper.message_type_style("alert")).to eq("bg-theme-alert")
       end
     end
 
