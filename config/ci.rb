7656b91d79231f22745bc562d5347d4d0616b88b
Taketo Takashima <t.taketo1113@gmail.com>
2025-09-23 23:05:33 +0900

    Update config/ci.rb to skip rubocop and fix rspec command

--- config/ci.rb
+++ config/ci.rb
@@ -3,11 +3,11 @@
 CI.run do
   step "Setup", "bin/setup --skip-server"
 
-  step "Style: Ruby", "bin/rubocop"
+  # step "Style: Ruby", "bin/rubocop"
 
   step "Security: Gem audit", "bin/bundler-audit"
   step "Security: Importmap vulnerability audit", "bin/importmap audit"
-  step "Tests: Rails", "bin/rails test"
+  step "Tests: Rails", "bin/rails spec"
   step "Tests: Seeds", "env RAILS_ENV=test bin/rails db:seed:replant"
 
   # Optional: set a green GitHub commit status to unblock PR merge.
