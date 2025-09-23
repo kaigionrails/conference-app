8eac398f8b258e886075131cc4a8500855f7886f
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-09-23 15:50:35 +0900

    [WIP]Live stream watching page

--- app/javascript/controllers/live_streams_controller.js
+++ app/javascript/controllers/live_streams_controller.js
@@ -2,18 +2,35 @@ import { Controller } from "@hotwired/stimulus";
 import Hls from "hls.js";
 
 export default class extends Controller {
-  static values = { url: String };
+  static values = {
+    url: String,
+    selectedTab: { type: String, default: "red" },
+  };
+
 
   connect() {
-    const videoElement = document.getElementById("video");
-    const videoSrc = this.urlValue;
-    if (Hls.isSupported()) {
-      const hls = new Hls();
-      hls.loadSource(videoSrc);
-      hls.attachMedia(videoElement);
-    } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
-      video.src = videoSrc;
+    const currentHash = (new URL(location.href)).hash.replace("#", "");
+    console.log("Current hash:", currentHash);
+    // Hall Red or Hall Blue
+    if (currentHash == "red" || currentHash == "blue") {
+      this.selectedTabValue = currentHash
     }
-    videoElement.play();
+    // this.updateCurrentTab();
+    // const videoElement = document.getElementById("video");
+    // const videoSrc = this.urlValue;
+    // if (Hls.isSupported()) {
+    //   const hls = new Hls();
+    //   hls.loadSource(videoSrc);
+    //   hls.attachMedia(videoElement);
+    // } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+    //   video.src = videoSrc;
+    // }
+    // videoElement.play();
+  }
+
+  updateCurrentTab({ params: { tab } }) {
+    if (tab) {
+      this.selectedTabValue = tab;
+    }
   }
 }
