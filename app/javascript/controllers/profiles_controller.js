daabecd1bcf1b1445225319e3cd61ac228aa0805
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-09-25 06:37:20 +0900

    Enable profile exchange (2025)
    Kaigi on Rails 2025!
    
    This reverts commit 360534fa116d55381744003bd407c5f06d49f1c4.

--- app/javascript/controllers/profiles_controller.js
+++ app/javascript/controllers/profiles_controller.js
@@ -41,13 +41,13 @@ export default class extends Controller {
 
   async showQrcode() {
     // Sure, unsecure! but enough ;)
-    // const unsecuredJwt = new jose.UnsecuredJWT({})
-    //   .setIssuedAt()
-    //   .setIssuer(this.usernameValue)
-    //   .setExpirationTime("3mins")
-    //   .encode();
+    const unsecuredJwt = new jose.UnsecuredJWT({})
+      .setIssuedAt()
+      .setIssuer(this.usernameValue)
+      .setExpirationTime("3mins")
+      .encode();
     const profileUrl = new URL(
-      `@${this.usernameValue}`,
+      `@${this.usernameValue}?token=${unsecuredJwt}`,
       this.baseUrlValue
     ).toString();
     const dataUrl = await QRCode.toDataURL(profileUrl, {
