005df7ad766a2031127598cd237c119d9f020edd
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-09-25 13:45:45 +0900

    Viewable video stream!

--- app/controllers/live_streams_controller.rb
+++ app/controllers/live_streams_controller.rb
@@ -6,7 +6,19 @@ class LiveStreamsController < ApplicationController
   # @rbs return: void
   def index
     @event = Event.find_by!(slug: params[:event_slug])
-    # @live_streams = CloudflareStreamLiveStream.where(event: @event)
+    live_streams = CloudflareStreamLiveStream.where(event: @event)
+    @live_streams = {
+      day1: {
+        red_ja: live_streams.detect { |s| s.name.include?("day1-red-ja") }&.stream_videos_raw_response&.dig("result", 0, "playback", "hls") || "",
+        blue_ja: live_streams.detect { |s| s.name.include?("day1-blue-ja") }&.stream_videos_raw_response&.dig("result", 0, "playback", "hls") || ""
+      },
+      day2: {
+        red_ja: live_streams.detect { |s| s.name.include?("day2-red-ja") }&.stream_videos_raw_response&.dig("result", 0, "playback", "hls") || "",
+        red_raw: live_streams.detect { |s| s.name.include?("day2-red-raw") }&.stream_videos_raw_response&.dig("result", 0, "playback", "hls") || "",
+        blue_ja: live_streams.detect { |s| s.name.include?("day2-blue-ja") }&.stream_videos_raw_response&.dig("result", 0, "playback", "hls") || ""
+      },
+      test: live_streams.detect { |s| s.name.include?("test") }&.stream_videos_raw_response&.dig("result", 0, "playback", "hls") || ""
+    }
   end
 
   # @rbs return: void
--- app/javascript/controllers/live_streams_controller.js
+++ app/javascript/controllers/live_streams_controller.js
@@ -2,30 +2,58 @@ import { Controller } from "@hotwired/stimulus";
 import Hls from "hls.js";
 
 export default class extends Controller {
-  // static values = {
-  //   url: String,
-  //   selectedTab: { type: String, default: "red" },
-  // };
-
+  static values = {
+    selectedTab: { type: String, default: "red" },
+    day1RedJa: { type: String, default: "" },
+    day1BlueJa: { type: String, default: "" },
+    day2RedJa: { type: String, default: "" },
+    day2RedRaw: { type: String, default: "" },
+    day2BlueJa: { type: String, default: "" },
+    test: { type: String, default: "" },
+  };
 
   connect() {
-    // const currentHash = (new URL(location.href)).hash.replace("#", "");
-    // console.log("Current hash:", currentHash);
-    // Hall Red or Hall Blue
-    // if (currentHash == "red" || currentHash == "blue") {
-    //   this.selectedTabValue = currentHash
-    // }
-    // this.updateCurrentTab();
-    // const videoElement = document.getElementById("video");
-    // const videoSrc = this.urlValue;
-    // if (Hls.isSupported()) {
-    //   const hls = new Hls();
-    //   hls.loadSource(videoSrc);
-    //   hls.attachMedia(videoElement);
-    // } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
-    //   video.src = videoSrc;
-    // }
-    // videoElement.play();
+    const video = document.getElementById("video");
+    const currentHash = new URL(location.href).hash.replace("#", "");
+    console.log("Current hash:", currentHash);
+
+    // Get today's date
+    const today = new Date();
+    const day = today.getDate();
+
+    if (day === 26) {
+      if (currentHash === "red") {
+        this.videoSrc = this.day1RedJaValue;
+        this.selectedTabValue = "red";
+      } else if (currentHash === "blue") {
+        this.videoSrc = this.day1BlueJaValue;
+        this.selectedTabValue = "blue";
+      } else {
+        console.warn("unknown day or tab value");
+      }
+    } else if (day === 27) {
+      console.log("It's day 2!");
+    }
+
+    if (currentHash === "test") {
+      this.videoSrc = this.testValue;
+    }
+
+    // Apply video source if set
+    if (this.videoSrc) {
+      const videoElement = document.getElementById("video");
+      const videoSrc = this.videoSrc;
+      if (Hls.isSupported()) {
+        const hls = new Hls();
+        hls.loadSource(videoSrc);
+        hls.attachMedia(videoElement);
+      } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+        video.src = videoSrc;
+      } else {
+        console.log("HLS is not supported in this browser");
+      }
+      // videoElement.play(); // Uncomment if you want autoplay
+    }
   }
 
   // updateCurrentTab({ params: { tab } }) {
